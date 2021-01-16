8ff92dc3e1610d279bccefd953d97d5d8f5ce88c
Kaitlyn Branagan <kaitlynwebb_46@hotmail.com>
2021-01-16 15:01:05 -0700

    working with my tutor today to fix my bugs

---
+++ routes/api.js
@@ -0,0 +1,72 @@
+const router = require("express").Router();
+const Workout = require("../models/Workout.js");
+
+
+
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+    .then(data => {
+        console.log(data)
+        res.json(data);
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+})
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+
+
+router.put("/api/workouts/:id", (req, res) => {
+    const id = req.params.id;
+    const body = req.body
+
+    Workout.findOneAndUpdate({ _id: id }, {$push:{ exercises: body }})
+    .then(data => {
+        console.log(data)
+        res.json(data);
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+})
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+    .then(data => {
+        console.log(data)
+        res.json(data);
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+})
+
+
+
+router.put("/api/workouts/:id", (req, res) => {
+    const id = req.params.id;
+    const body = req.body
+
+    Workout.findOneAndUpdate({ id }, {$push:{ exercises: body }},
+        {new: true})
+    .then(data => {
+        console.log(data)
+        res.json(data);
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://katers0259:Coolie23@fitness-app.99qpm.mongodb.net/katers0259?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/Fitness-app',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/homeRoutes.js"));
+// app.use(require("./routes/index.js"));
+
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
 
